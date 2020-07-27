6f22a9cab75e61f046a741443614e722a62f0606
Jonatan del Valle <delvallejonatan@gmail.com>
2020-07-26 22:21:27 -0300

    Updates

---
+++ precache-manifest.044bb78628bc0e26238036b5ab02ba8f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd40a950ddb5cbf07ae528168fac73a",
+    "url": "/payment-v2/index.html"
+  },
+  {
+    "revision": "b61dd8a4fc92b988aace",
+    "url": "/payment-v2/static/js/2.e17bfc92.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/payment-v2/static/js/2.e17bfc92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f17f3117366c8777ec3",
+    "url": "/payment-v2/static/js/main.f28ae2d0.chunk.js"
+  },
+  {
+    "revision": "64d96c9506fa62a665b8",
+    "url": "/payment-v2/static/js/runtime-main.02d0f452.js"
+  },
+  {
+    "revision": "a05687c901eba5e4963a255938c70de1",
+    "url": "/payment-v2/static/media/aidhelplogo.a05687c9.svg"
+  },
+  {
+    "revision": "60df7f86c41806b406b392b55068b94a",
+    "url": "/payment-v2/static/media/arrow.60df7f86.svg"
+  },
+  {
+    "revision": "a6cafb410fdc211f0a229a5a83f88fb4",
+    "url": "/payment-v2/static/media/heart.a6cafb41.svg"
+  },
+  {
+    "revision": "588684db5c1f5df51154955d29beca49",
+    "url": "/payment-v2/static/media/tickImg.588684db.svg"
+  },
+  {
+    "revision": "4d00d1b8401f48f64bca0a651c3acd54",
+    "url": "/payment-v2/static/media/titleImg.4d00d1b8.svg"
+  }
+]);
